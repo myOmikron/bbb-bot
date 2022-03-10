a2f25b6b797cebeaf2b6d41d4eb7a80796300121
gammelalf <31348226+gammelalf@users.noreply.github.com>
2022-03-10 14:40:52 +0100

    Use root logger

--- bot_script.py
+++ bot_script.py
@@ -14,7 +14,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 
-logger = logging.getLogger(__file__)
+logger = logging.getLogger()
 
 
 def main():

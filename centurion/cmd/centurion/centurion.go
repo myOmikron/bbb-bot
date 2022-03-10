25f7f8f6e3b25a07d8a65c027bf3a7c437386d29
myOmikron <git@omikron.dev>
2022-03-10 17:25:11 +0100

    First draft of working attack

--- centurion/cmd/centurion/centurion.go
+++ centurion/cmd/centurion/centurion.go
@@ -28,6 +28,22 @@ func main() {
 			fmt.Println(err.Error())
 			os.Exit(1)
 		}
+		if len(config.CenturiaUris) == 0 {
+			fmt.Println("No centurias available, exiting ...")
+			os.Exit(0)
+		}
+		if config.BBBServerUri == "" {
+			fmt.Println("BBB server uri is empty, exiting ...")
+			os.Exit(0)
+		}
+		if config.BBBSecret == "" {
+			fmt.Println("BBB secret is empty, exiting ...")
+			os.Exit(0)
+		}
+		if config.RCPSecret == "" {
+			fmt.Println("RCP Secret is empty, exiting ...")
+			os.Exit(0)
+		}
 	}
 
 	rcpConfig := &gorcp.RCPConfig{
@@ -38,6 +54,9 @@ func main() {
 	parser := argparse.NewParser("centurion", "Commander of bbb-bots", nil)
 
 	attackParser := parser.AddCommand("attack", "Start the attack", nil)
+	armySize := attackParser.Int("", "armySize", &argparse.Option{Positional: true, Required: true})
+	senderCount := attackParser.Int("", "sender-count", &argparse.Option{Default: "2"})
+
 	withdrawParser := parser.AddCommand("withdraw", "Withdraw the attack", nil)
 
 	if err := parser.Parse(nil); err != nil {
@@ -52,16 +71,38 @@ func main() {
 			requestMap["bbb_server_uri"] = config.BBBServerUri
 			requestMap["bbb_secret"] = config.BBBSecret
 
-			checksum := gorcp.GetChecksum(&requestMap, "startBot", rcpConfig)
-			requestMap["checksum"] = checksum
-			if requestBody, err := json.Marshal(requestMap); err != nil {
-				fmt.Println(err.Error())
-				os.Exit(1)
-			} else {
-				for _, centuria := range config.CenturiaUris {
-					hc.Post(centuria+"/api/v1/startBot", "application/json", bytes.NewReader(requestBody))
+			for i := 0; i < *armySize; i++ {
+				if i < *senderCount {
+					requestMap["sender"] = "true"
+				}
+				checksum := gorcp.GetChecksum(&requestMap, "startBot", rcpConfig)
+				requestMap["checksum"] = checksum
+				if requestBody, err := json.Marshal(requestMap); err != nil {
+					fmt.Println(err.Error())
+					os.Exit(1)
+				} else {
+					targetIdx := i % len(config.CenturiaUris)
+					centuria := config.CenturiaUris[targetIdx]
+					if post, err := hc.Post(centuria+"/api/v1/startBot", "application/json", bytes.NewReader(requestBody)); err != nil {
+						fmt.Println(err.Error())
+						os.Exit(1)
+					} else {
+						if post.StatusCode != 200 {
+							var body []byte
+							_, err := post.Body.Read(body)
+							if err != nil {
+								break
+							}
+							var res []interface{}
+							if err := json.Unmarshal(body, &res); err != nil {
+								break
+							}
+							fmt.Printf("Status: %d: %v\n", post.StatusCode, res)
+						}
+					}
 				}
 			}
+			fmt.Printf("Spawned %d soldiers in meeting %s\n", *armySize, "test")
 
 		case withdrawParser.Invoked:
 		}
--- centurion/go.mod
+++ centurion/go.mod
@@ -4,5 +4,5 @@ go 1.17
 
 require (
 	github.com/hellflame/argparse v1.7.4
-	github.com/myOmikron/gorcp v0.0.0-20220310150901-5eca9936e595
+	github.com/myOmikron/gorcp v0.0.0-20220310155149-c29c112ded56
 )
